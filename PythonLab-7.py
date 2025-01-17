d8eeb563bee678e52bbb42a734b998b96daba1e2
mirudulaav <mirudulaaa@gmail.com>
2025-01-17 17:43:20 +0530

    PythonLab-7.py

--- PythonLab-7.py
+++ PythonLab-7.py
@@ -1,15 +1,15 @@
 #pythonlab7
-a=input("Enter the required string:")
-a.count(a)
-def count(a):
-    upper=0
-    lower=0
-    for i in a:
+def count(s):
+    upper_count=0
+    lower_count=0
+    for i in s:
         if i.isupper():
-            upper+= 1
+            upper_count+= 1
         elif i.islower():
-            lower+=1
-            
-print(f"Count of lowercases: {lower}")
-print(f"Count of uppercases: {upper}")
+            lower_count+=1
+    return upper_count, lower_count
+s=input("Enter string:")
+upper_count, lower_count= count(s)
+print(f"Count of lowercases: {lower_count}")
+print(f"Count of uppercases: {upper_count}")
             
