e7775981962424e4d2a77962b437856aebb839d0
mirudulaav <mirudulaaa@gmail.com>
2025-01-17 17:49:52 +0530

    PythonLab-4.py

--- PythonLab-4.py
+++ PythonLab-4.py
@@ -1,7 +1,16 @@
 # Fibonacci series
-num_terms = int(input("Enter the number of terms: "))
-a, b = 0, 1
-print("Fibonacci sequence:")
-for _ in range(num_terms):
-    print(a, end=" ")
-    a, b = b, a + b
+num_terms=int(input("Enter number of terms:"))
+n1=0
+n2=1
+count=0
+if num_terms<=0:
+    print("Enter a positive number")
+elif num_terms==1:
+    print(n1)
+else:
+    while count<num_terms:
+        print(n1, end=" ")
+        n3=n1+n2
+        n1=n2
+        n2=n3
+        count+=1
